d6b92c5c16b9a1b10d8c630bce45c21f4ade8b54
hungsean <hungsean0419@gmail.com>
2025-01-23 17:18:49 +0800

    feat(ui): implement header component
    Add header component for roomchat interface to enhance UI structure.

--- src/pages/roomChat.tsx
+++ src/pages/roomChat.tsx
@@ -1,8 +1,37 @@
+import { Avatar } from "@/components/ui/avatar";
+import { Box, Center, Flex, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+
 const RoomChat = () => {
+    const [roomName, setRoomName] = useState('taki');
+    const [roomAvatarSrc, setRoomAvatarSrc] = useState('https://i.imgur.com/LtR2mmT.png');
+
+    const ChatHeader = () => {
+        return (
+            <Box
+                height={'100px'}
+                backgroundColor={'gray.900'}
+                borderRadius={'lg'}
+                margin={'20px'}
+                padding={'20px'}
+            >
+                <Flex direction={'column'} justify={'center'} height={'100%'}>
+                    <Flex justify={'flex-start'} gap={'4'}>
+                        <Avatar name={roomName} src={roomAvatarSrc} size={'lg'}></Avatar>
+                        <Center>
+                            <Heading>{roomName}</Heading>
+                        </Center>
+                    </Flex>
+                </Flex>
+            </Box>
+            
+        );
+    };
+
+
+
     return (
-        <div>
-            roomchat
-        </div>
+        <ChatHeader/>
     )
 }
 export default RoomChat
