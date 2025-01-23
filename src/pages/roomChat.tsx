8709f6cde9b6fbc8f77b79cdfd35bd8fcb870f60
hungsean <hungsean0419@gmail.com>
2025-01-23 20:51:06 +0800

    feat(ui): initialize message unit prototype
    Initial setup for message unit functionality. Currently in early prototype phase with basic structure.

--- src/pages/roomChat.tsx
+++ src/pages/roomChat.tsx
@@ -1,19 +1,23 @@
 import { Avatar } from "@/components/ui/avatar";
-import { Box, Center, Flex, Heading } from "@chakra-ui/react";
+import { Box, Center, Flex, For, Heading, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
 const RoomChat = () => {
     const [roomName, setRoomName] = useState('taki');
     const [roomAvatarSrc, setRoomAvatarSrc] = useState('https://i.imgur.com/LtR2mmT.png');
 
+    const roomChatDesignStyle = {
+        backgroundColor: 'gray.900',
+        borderRadius: 'lg',
+        // margin: '20px',
+        padding: '20px'
+    }
+
     const ChatHeader = () => {
         return (
             <Box
                 height={'100px'}
-                backgroundColor={'gray.900'}
-                borderRadius={'lg'}
-                margin={'20px'}
-                padding={'20px'}
+                {...roomChatDesignStyle}
             >
                 <Flex direction={'column'} justify={'center'} height={'100%'}>
                     <Flex justify={'flex-start'} gap={'4'}>
@@ -28,10 +32,52 @@ const RoomChat = () => {
         );
     };
 
+    const test_chat_content = [
+        "this is a test message ",
+        "",
+        "it can text here ",
+        "hi!there!"
+    ]
+
+    const MessageUnit = () => {
+        return (
+            <Box
+                padding={'10px'}
+            >
+                <Flex gap={'4'}> 
+                    <Avatar></Avatar>
+                    <Flex direction={'column'} gap={'2'}>
+                        <Flex gap={'2'} alignItems={'baseline'}>
+                            <Text textStyle={'2xl'}>username</Text>
+
+                            <Text textStyle={'xs'}>time</Text>
+                        </Flex>
+                        <Box>
+                            <For each={test_chat_content}>
+                                {(item, index) => <Text>{item}</Text>}
+                            </For>
+                        </Box>
+                        
+
+                    </Flex>
+                </Flex>
+            </Box>
+        )
+    }
+
 
 
     return (
-        <ChatHeader/>
+        <Flex direction={'column'}>
+            <ChatHeader />
+            <Box borderWidth={'1px'}>
+                <Text>message unit test </Text>
+                <MessageUnit />
+                <MessageUnit />
+                <MessageUnit />
+            </Box>
+        </Flex>
+        
     )
 }
 export default RoomChat
