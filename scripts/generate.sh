cdac91a78b40d8882ddf02ee4d13a2642dfa87c7
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-01 00:39:47 +0800

    refactor: change to grpc-web typescript

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,13 +1,13 @@
-PROTOC_GEN_TS_PATH="node_modules/.bin/protoc-gen-ts"
 PROTOC_GEN_JS_PATH="node_modules/.bin/protoc-gen-js"
+PROTOC_GEN_GRPC_WEB_PATH="path/to/protoc-gen-grpc-web"
 PROTO_PATH="src/protos/"
 OUT_DIR="src/proto-generated"
 PROTO_TARGET="$(find src/protos/nori -name "*.proto")"
 
 protoc \
     --proto_path="${PROTO_PATH}" \
-    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --plugin="protoc-gen-js=${PROTOC_GEN_JS_PATH}" \
-    --js_out="import_style=es6,binary:${OUT_DIR}" \
-    --ts_out="service=grpc-web:${OUT_DIR}" \
+    --plugin="protoc-gen-grpc-web=${PROTOC_GEN_GRPC_WEB_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --grpc-web_out=import_style=typescript,mode=grpcweb:${OUT_DIR} \
     ${PROTO_TARGET}
