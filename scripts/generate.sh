12650b5b8c52bc1340c4208e93921e4bdebbf048
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-01-31 21:31:33 +0800

    fix: generate ts problem

---
+++ scripts/generate.sh
@@ -0,0 +1,13 @@
+PROTOC_GEN_TS_PATH="node_modules/.bin/protoc-gen-ts"
+PROTOC_GEN_JS_PATH="node_modules/.bin/protoc-gen-js"
+PROTO_PATH="src/protos/"
+OUT_DIR="src/proto-generated"
+PROTO_TARGET="$(find src/protos/nori -name "*.proto")"
+
+protoc \
+    --proto_path="${PROTO_PATH}" \
+    --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
+    --plugin="protoc-gen-js=${PROTOC_GEN_JS_PATH}" \
+    --js_out="import_style=commonjs,binary:${OUT_DIR}" \
+    --ts_out="service=grpc-web:${OUT_DIR}" \
+    ${PROTO_TARGET}
