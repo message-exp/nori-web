1d3817a90aa786602513c6878a9bc0ce7e412de8
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-01 00:41:03 +0800

    fix: change to grpc-web
    reference: https://github.com/grpc/grpc-web?tab=readme-ov-file#typescript-support

--- src/api/user/get_user.ts
+++ src/api/user/get_user.ts
@@ -1,9 +1,17 @@
-import { UserServiceClient } from "../../proto-generated/nori/v0/user/user_service_pb_service";
-import { UserId } from "../../proto-generated/nori/v0/user/user_id_pb";
+import * as grpcWeb from 'grpc-web';
+import { UserServiceClient } from "@/proto-generated/nori/v0/user/User_serviceServiceClientPb";
+import { UserId } from "@/proto-generated/nori/v0/user/user_id_pb";
+import { User } from "@/proto-generated/nori/v0/user/user_pb";
 
-const userClient = new UserServiceClient("https://my.grpc/server");
-const req = new UserId();
-req.setUserId("");
-userClient.getUser(req, (err, user) => {
-  /* ... */
+const userService = new UserServiceClient("https://localhost:8080", null, null);
+
+const request = new UserId();
+request.setUserId("");
+
+const call = userService.getUser(request, {'custom-header-1': 'value1'},
+  (err: grpcWeb.RpcError, response: User) => {
+    console.log("test");
+  });
+call.on('status', (status: grpcWeb.Status) => {
+  // ...
 });
