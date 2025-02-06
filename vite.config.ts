de0322de8067e034fb6295e9a5eaad3ae530fb80
hungsean <hungsean0419@gmail.com>
2025-02-06 14:33:07 +0800

    feat: add React compiler for optimizing rendering
    - Add babel-plugin-react-compiler for better React runtime performance
    - Change bundler from react-swc to react plugin to support the compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path';
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      }
+    }),
+    tsconfigPaths()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
