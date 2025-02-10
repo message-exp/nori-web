1f8a4a85eefeb70721759f79fe7bd20aff057d5d
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-10 13:10:17 +0800

    fix: eslint error and warnings

--- src/api/user/user-service.ts
+++ src/api/user/user-service.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
+import { create } from "@bufbuild/protobuf";
+import { Empty } from "@bufbuild/protobuf/wkt";
 import { UserService } from "@/proto-generated/nori/v0/user/user_service_pb";
 import { UserEmailPasswordLoginSchema } from "@/proto-generated/nori/v0/user/user_login_pb";
-import { create } from "@bufbuild/protobuf";
 
 
 const transport = createConnectTransport({
@@ -13,7 +14,7 @@ const transport = createConnectTransport({
 // definition with the transport.
 const client = createClient(UserService, transport);
 
-export const login = async (input_email: string, input_password: string): Promise<any> => {
+export const login = async (input_email: string, input_password: string): Promise<Empty> => {
     console.info("get login info");
     console.info("email: ", input_email);
     console.info("password: ", input_password);
@@ -21,18 +22,18 @@ export const login = async (input_email: string, input_password: string): Promis
     const loginRequest = create(UserEmailPasswordLoginSchema, {
         email: input_email,
         password: input_password
-    })
+    });
 
     // 執行登入請求
     try {
-        // const response = await client.login(loginRequest);
-        const response = "test_OK[if you see this, it just a test]";
+        const response = await client.login(loginRequest);
+        // const response = "test_OK[if you see this, it just a test]";
         console.log("登入成功", response);
         return response;
     } catch (error) {
         console.error("登入失敗", error);
         throw error;
     }
-}
+};
 
 
