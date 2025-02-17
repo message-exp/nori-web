55c743bb29df5cf11e6accca195c41eabd48b8a2
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-02-17 22:42:12 +0800

    feat: auth interceptor

---
+++ src/api/auth-interceptor.ts
@@ -0,0 +1,20 @@
+import { Interceptor } from "@connectrpc/connect";
+import { storage } from "@/utils/storage/user-storage";
+
+export const authInterceptor: Interceptor = (next) => async (req) => {
+    // get token from storage
+    const storageUtil = storage.getUserAuth();
+    if (!storageUtil) {
+        throw new Error("User is not authenticated");
+    }
+    const token = storageUtil?.tokenpair.accessToken?.accessToken;
+    
+    // set token in request header
+    if (token) {
+        const tokenString = new TextDecoder().decode(token); // convert bytes to string
+        req.header.set("authorization", tokenString);
+    }
+
+    // continue with request
+    return await next(req);
+};
--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,8 @@
 import { createGrpcWebTransport } from "@connectrpc/connect-web";
 import config from "@/utils/config";
+import { authInterceptor } from "./auth-interceptor";
 
 export const transport = createGrpcWebTransport({
     baseUrl: config.backendUrl,
+    interceptors: [authInterceptor],
 });
