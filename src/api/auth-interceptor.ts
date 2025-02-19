7367ef1546d1e1b174321052cad012a2a6448e25
ErEr <jeffliao92316046@gmail.com>
2025-02-19 10:20:46 +0800

    Refactor: update token handling and improve user authentication storage

--- src/api/auth-interceptor.ts
+++ src/api/auth-interceptor.ts
@@ -2,18 +2,18 @@ import { Interceptor } from "@connectrpc/connect";
 import { storage } from "@/utils/storage/user-storage";
 
 export const authInterceptor: Interceptor = (next) => async (req) => {
-    // get token from storage
-    const storageUtil = storage.getUserAuth();
-    if (!storageUtil) {
-        return await next(req);
-    }
-    const token = storageUtil?.tokenpair.accessToken?.accessToken;
-    
-    // set token in request header
-    if (token) {
-        req.header.set("authorization", `Bearer ${token}`);
-    }
-
-    // continue with request
+  // get token from storage
+  const storageUtil = storage.getUserAuth();
+  if (!storageUtil) {
     return await next(req);
+  }
+  const token = storageUtil?.tokenPair.accessToken?.accessToken;
+    
+  // set token in request header
+  if (token) {
+    req.header.set("authorization", `Bearer ${token}`);
+  }
+
+  // continue with request
+  return await next(req);
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,177 +7,177 @@ import { storage } from "@/utils/storage/user-storage";
 import { getUserIdFromAccessToken } from "@/utils/jwt";
 
 const LoginPage = () => {
-    const [flag, setFlag] = useState(1);
-    if (flag) {
-        return (
-            <Welcome setFlag={setFlag} />
-        );
-    } else {
-        return (
-            <Login/>
-        );
-    }
+  const [flag, setFlag] = useState(1);
+  if (flag) {
+    return (
+      <Welcome setFlag={setFlag} />
+    );
+  } else {
+    return (
+      <Login/>
+    );
+  }
 };
 const Welcome = ({ setFlag }: { setFlag: (value: number) => void }) => {
-    return (
-        <Center height="100vh" bg="#1e1e1e" color="white">
-            <VStack>
-                <Image
-                    src="main.jpg"
-                    boxSize="96px"
-                    borderRadius="full"
-                    fit="cover"
-                    alt="main"
-                />
-                <Heading as="h1" size="lg">
+  return (
+    <Center height="100vh" bg="#1e1e1e" color="white">
+      <VStack>
+        <Image
+          src="main.jpg"
+          boxSize="96px"
+          borderRadius="full"
+          fit="cover"
+          alt="main"
+        />
+        <Heading as="h1" size="lg">
                     Welcome to Nori
-                </Heading>
-                <Button
-                    colorScheme="gray"
-                    onClick={() => setFlag(0)}
-                >
+        </Heading>
+        <Button
+          colorScheme="gray"
+          onClick={() => setFlag(0)}
+        >
                     Log in
-                </Button>
-            </VStack>
-        </Center>
+        </Button>
+      </VStack>
+    </Center>
 
-    );
+  );
 
 };
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [account, setAccount] = useState("");
-    const [password, setPassword] = useState("");
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [isEmailError, setIsEmailError] = useState(false);
-    const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [isEmailError, setIsEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
 
-    const [isPasswordError, setIsPasswordError] = useState(false);
-    const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [isPasswordError, setIsPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
 
-    const [textErrorMessage, setTextErrorMessage] = useState("");
+  const [textErrorMessage, setTextErrorMessage] = useState("");
 
-    const [isLoginLoading, setIsLoginLoading] = useState(false);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
 
-    function isEmpty(str: string | null | undefined): boolean {
-        return !str || str.trim() === "";
+  function isEmpty(str: string | null | undefined): boolean {
+    return !str || str.trim() === "";
+  }
+
+  const loginFunc = async () => {
+    console.log("account: " + account);
+    console.log("password: " + password);
+    /* 串接API */
+    setTextErrorMessage("");
+    setIsLoginLoading(true);
+    let isInputEmpty = false;
+    if (isEmpty(account)) {
+      setEmailErrorMessage("account empty");
+      setIsEmailError(true);
+      isInputEmpty = true;
+    }
+    else {
+      setEmailErrorMessage("");
+      setIsEmailError(false);
     }
 
-    const loginFunc = async () => {
-        console.log("account: " + account);
-        console.log("password: " + password);
-        /* 串接API */
-        setTextErrorMessage("");
-        setIsLoginLoading(true);
-        let isInputEmpty = false;
-        if (isEmpty(account)) {
-            setEmailErrorMessage("account empty");
-            setIsEmailError(true);
-            isInputEmpty = true;
-        }
-        else {
-            setEmailErrorMessage("");
-            setIsEmailError(false);
-        }
+    if (isEmpty(password)) {
+      setPasswordErrorMessage("password empty");
+      setIsPasswordError(true);
+      isInputEmpty = true;
+    }
+    else {
+      setPasswordErrorMessage("");
+      setIsPasswordError(false);
+    }
 
-        if (isEmpty(password)) {
-            setPasswordErrorMessage("password empty");
-            setIsPasswordError(true);
-            isInputEmpty = true;
-        }
-        else {
-            setPasswordErrorMessage("");
-            setIsPasswordError(false);
-        }
+    if (isInputEmpty) {
+      setTextErrorMessage("");
+      setIsLoginLoading(false);
+      return;
+    }
 
-        if (isInputEmpty) {
-            setTextErrorMessage("");
-            setIsLoginLoading(false);
-            return;
-        }
+    try {
+      const tokenPair = await login(account, password);
+      console.log("get token pair: ", tokenPair);
+      const userid = getUserIdFromAccessToken(tokenPair.accessToken); 
+      console.log("get userid: ", userid);
 
-        try {
-            const tokenpair = await login(account, password);
-            console.log("get token pair: ", tokenpair);
-            const userid = getUserIdFromAccessToken(tokenpair.accessToken); 
-            console.log("get userid: ", userid);
+      storage.setUserAuth({ userId: userid.id, tokenPair: tokenPair });
 
-            storage.setUserAuth({ userId: Number(userid.id), tokenpair: tokenpair });
+      setTextErrorMessage("");
+      setIsLoginLoading(false);
 
-            setTextErrorMessage("");
-            setIsLoginLoading(false);
-
-            navigate("/roomlist");
-        } catch (error) {
-            console.log("get error: ", error);
-            setTextErrorMessage("email or password error");
-            setIsLoginLoading(false);
-        }
+      navigate("/roomlist");
+    } catch (error) {
+      console.log("get error: ", error);
+      setTextErrorMessage("email or password error");
+      setIsLoginLoading(false);
+    }
         
 
-        //below just for demo
-        // if (account == "test" && password == "test")
-        // {
-        //     setTextErrorMessage("");
-        //     navigate("/roomlist");
-        // }
-        // else
-        // {
-        //     setTextErrorMessage("email or password error");
-        // }
+    //below just for demo
+    // if (account == "test" && password == "test")
+    // {
+    //     setTextErrorMessage("");
+    //     navigate("/roomlist");
+    // }
+    // else
+    // {
+    //     setTextErrorMessage("email or password error");
+    // }
         
-    };
+  };
 
 
 
-    return (
-        <Center height="100vh" bg="#1e1e1e" color="white">
-            <VStack width="300px">
-                <Heading as="h1" size="lg">Login</Heading>
-                <Field.Root invalid={isEmailError}>
-                    <Field.Label>Email</Field.Label>
-                    <Input
-                        placeholder="Email"
-                        value={account}
-                        onChange={(e) => setAccount(e.target.value)}
-                        bg="gray.700"
-                        color="white"
-                    />
-                    <Field.ErrorText>{ emailErrorMessage }</Field.ErrorText>
-                </Field.Root>
+  return (
+    <Center height="100vh" bg="#1e1e1e" color="white">
+      <VStack width="300px">
+        <Heading as="h1" size="lg">Login</Heading>
+        <Field.Root invalid={isEmailError}>
+          <Field.Label>Email</Field.Label>
+          <Input
+            placeholder="Email"
+            value={account}
+            onChange={(e) => setAccount(e.target.value)}
+            bg="gray.700"
+            color="white"
+          />
+          <Field.ErrorText>{ emailErrorMessage }</Field.ErrorText>
+        </Field.Root>
 
-                <Field.Root invalid={isPasswordError}>
-                    <Field.Label>Password</Field.Label>
-                    <PasswordInput
-                        placeholder="Password"
-                        // type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        bg="gray.700"
-                        color="white"
-                    />
-                    <Field.ErrorText>{passwordErrorMessage}</Field.ErrorText>
-                </Field.Root>
+        <Field.Root invalid={isPasswordError}>
+          <Field.Label>Password</Field.Label>
+          <PasswordInput
+            placeholder="Password"
+            // type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            bg="gray.700"
+            color="white"
+          />
+          <Field.ErrorText>{passwordErrorMessage}</Field.ErrorText>
+        </Field.Root>
                 
                 
-                <Button
-                    width="full"
-                    onClick={loginFunc}
-                    loading={isLoginLoading}
-                >
+        <Button
+          width="full"
+          onClick={loginFunc}
+          loading={isLoginLoading}
+        >
                     Login
-                </Button>
+        </Button>
 
-                <Text color={"red.400"}>{ textErrorMessage }</Text>
+        <Text color={"red.400"}>{ textErrorMessage }</Text>
 
 
-                <Button onClick={() => navigate("/signup")} textDecor={"underline"} variant={"plain"}>
+        <Button onClick={() => navigate("/signup")} textDecor={"underline"} variant={"plain"}>
                     Signup if you don't have an account
-                </Button>
-            </VStack>
-        </Center>
-    );
+        </Button>
+      </VStack>
+    </Center>
+  );
 };
 
 export default LoginPage;
