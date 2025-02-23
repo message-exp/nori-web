8a56f6b727378d6a0c6b024617b82e56a44f2c49
barney9211 <barney9211@gmail.com>
2025-02-23 18:35:54 +0800

    refactor: change getRoomName logic with if-else

--- src/hooks/room-list/get-room-name.ts
+++ src/hooks/room-list/get-room-name.ts
@@ -1,7 +1,9 @@
 import { RoomBasicInfoResponse } from "@/proto-generated/nori/v0/room/room_basic_info_response_pb";
 
-
 export const getRoomName = (room: RoomBasicInfoResponse): string => {
-  return room.name.case === "sharedName" ? room.name.value :
-    room.name.case === "customName" ? room.name.value : "";
+  if (room.name.case === "sharedName" || room.name.case === "customName") {
+    return room.name.value;
+  } else {
+    return "";
+  }
 };
