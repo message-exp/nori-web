ef11aa4cd35a8b366dd006f135fef3fa5008625c
ErEr <jeffliao92316046@gmail.com>
2025-03-07 15:15:13 +0800

    feat: implement infinite scroll for chat messages with loading indicators

--- src/components/room-chat/ChatBody.tsx
+++ src/components/room-chat/ChatBody.tsx
@@ -1,15 +1,38 @@
-import { Box, Flex, For } from "@chakra-ui/react";
+import { Box, Flex, For, Spinner } from "@chakra-ui/react";
 import { Message } from "@/proto-generated/nori/v0/message/message_pb";
+import { useInView } from "react-intersection-observer";
 import MessageUnit from "@/components/room-chat/MessageUnit";
+import { useEffect } from "react";
 
 interface ChatBodyProps {
   chatMessages: Message[];
+  onLoadOlder: () => void;
+  onLoadNewer: () => void;
+  isLoadingOlder: boolean;
+  isLoadingNewer: boolean;
 }
 function ChatBody(props: Readonly<ChatBodyProps>) {
-  const { chatMessages } = props;
+  const { chatMessages, onLoadOlder, onLoadNewer, isLoadingOlder, isLoadingNewer } = props;
+  const { ref: topRef, inView: topInView } = useInView();
+  const { ref: bottomRef, inView: bottomInView } = useInView();
+
+  useEffect(() => {
+    if (topInView) {
+      onLoadOlder();
+    }
+  }, [topInView, onLoadOlder]);
+  
+  useEffect(()=>{
+    if (bottomInView){
+      onLoadNewer();
+    }
+  },[]);
+
   return (
     <Box height={"100%"}>
       <Flex direction={"column"} maxHeight={"100%"}>
+        <Box ref={topRef}/>
+        {isLoadingOlder && <Spinner size="sm" alignSelf="center" />}
         <For each={chatMessages}>
           {(message) => (
             <MessageUnit
@@ -25,6 +48,8 @@ function ChatBody(props: Readonly<ChatBodyProps>) {
             />
           )}
         </For>
+        {isLoadingNewer && <Spinner size="sm" alignSelf="center" />}
+        <Box ref={bottomRef}/>
       </Flex>
     </Box>
   );
