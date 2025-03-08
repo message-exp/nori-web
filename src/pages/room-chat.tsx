eb098587025f7568191fc86730d541346abadbd7
ErEr <jeffliao92316046@gmail.com>
2025-03-08 21:51:17 +0800

    feat: implement message sending and receiving in RoomChat component

--- src/pages/room-chat.tsx
+++ src/pages/room-chat.tsx
@@ -1,29 +1,62 @@
 import { Box, Flex } from "@chakra-ui/react";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import ChatHeader from "@/components/room-chat/ChatHeader";
 import ChatFooter from "@/components/room-chat/ChatFooter";
 import ChatBody from "@/components/room-chat/ChatBody";
 import { RoomMembersProvider } from "@/contexts/room-chat/RoomMembersProvider";
 import { storage } from "@/utils/storage/user-storage";
-import { Room } from "@/proto-generated/nori/v0/room/room_pb";
-import { Message } from "@/proto-generated/nori/v0/message/message_pb";
-import { GetMessage } from "@/api/message/message-service";
-// import { GetUser } from "@/api/user/user-service";
-import { User } from "@/proto-generated/nori/v0/user/account/user_pb";
+import { Message, MessageSchema } from "@/proto-generated/nori/v0/message/message_pb";
+import { GetLatestMessage, SendMessage } from "@/api/message/message-service";
+import { useParams } from "react-router";
+import { create } from "@bufbuild/protobuf";
+import { UserIdSchema } from "@/proto-generated/nori/v0/user/user_id_pb";
 
-interface RoomMembers {
-  [userId: string]: User;
-}
 const RoomChat = () => {
   const [roomName, setRoomName] = useState("taki");
   const [roomAvatarSrc, setRoomAvatarSrc] = useState(
     "https://i.imgur.com/LtR2mmT.png"
   );
-  // const [currentUser, setCurrentUser] = useState<User>();
-  // const [currentRoom, setCurrentRoom] = useState<Room>();
+  const currentRoomId = BigInt(useParams().roomId ?? "");
+  const currentUserId = storage.getUserId() ?? BigInt(0);
   const [chatMessages, setChatMessages] = useState<Message[]>([]);
-  // const [roomMembers, setRoomMembers] = useState<RoomMembers>({});
+  const [isSending, setIsSending] = useState<boolean>(false);
+  const [inputMessage, setInputMessage] = useState<string>("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const messageIterator = GetLatestMessage(currentRoomId, currentUserId);
+        for await (const msg of messageIterator) {
+          console.log("Received message:", msg);
+          setChatMessages((prev) => [...prev, msg]);
+        }
+      } catch (err: any) {
+        console.error("Error during manual test:", err);
+      }
+    })();
+  }, []);
+  const sendMessage = async () => {
+    if (!inputMessage) {
+      throw Error("no message");
+    }
+    try{
+      setIsSending(true);
+      await SendMessage(
+        currentRoomId,
+        currentUserId,
+        inputMessage
+      );
+      setChatMessages((prev)=>([...prev, create(MessageSchema, {
+        author: create(UserIdSchema, {
+          id: currentUserId ?? BigInt(0),
+        }),
+        text: inputMessage,
+      })]));
+    }finally{
+      setIsSending(false);
+    } 
+  };
 
   return (
     <RoomMembersProvider>
@@ -32,7 +65,12 @@ const RoomChat = () => {
         <Box flex={"1"} overflowY={"auto"}>
           <ChatBody chatMessages={chatMessages}></ChatBody>
         </Box>
-        <ChatFooter />
+        <ChatFooter
+          sendMessage={sendMessage}
+          setInputMessage={setInputMessage}
+          isSending={isSending}
+          inputMessage={inputMessage}
+        />
       </Flex>
     </RoomMembersProvider>
   );
