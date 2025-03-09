6b017250982241ccda49dd1cc854cb02193bb8eb
ErEr <jeffliao92316046@gmail.com>
2025-03-09 15:12:22 +0800

    feat: wrap App component with RoomMembersProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider as UIProvider } from "@/components/ui/provider";
+import { RoomMembersProvider } from "./contexts/room-chat/RoomMembersProvider.tsx";
 // import './index.css'
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UIProvider>
-      <App />
-    </UIProvider>
+    <RoomMembersProvider>
+      <UIProvider>
+        <App />
+      </UIProvider>
+    </RoomMembersProvider>
   </StrictMode>
 );
