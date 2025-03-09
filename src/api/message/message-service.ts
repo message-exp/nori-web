a7d2846acbc2677b81445d1bf7419123cdaf7693
ErEr <jeffliao92316046@gmail.com>
2025-03-09 15:12:37 +0800

    feat: implement GetHistoryMessage function

--- src/api/message/message-service.ts
+++ src/api/message/message-service.ts
@@ -1,19 +1,20 @@
-import { createClient, Code, ConnectError } from "@connectrpc/connect";
+import { createClient } from "@connectrpc/connect";
 import { create } from "@bufbuild/protobuf";
 import { MessageService } from "@/proto-generated/nori/v0/message/message_service_pb";
 import {
   Message,
-  MessageSchema,
 } from "@/proto-generated/nori/v0/message/message_pb";
 import { RoomIdSchema } from "@/proto-generated/nori/v0/room/room_id_pb";
 import { UserIdSchema } from "@/proto-generated/nori/v0/user/user_id_pb";
 import {
   GetLatestMessageRequestSchema,
   GetHistoryMessageRequestSchema,
+  Direction,
 } from "@/proto-generated/nori/v0/message/get_message_requests_pb";
 import { SendMessageRequestSchema } from "@/proto-generated/nori/v0/message/send_message_request_pb";
 import { transport } from "@/api/client";
 import { MessageId } from "@/proto-generated/nori/v0/message/message_id_pb";
+import { MessageList } from "@/proto-generated/nori/v0/message/message_list_pb";
 
 const client = createClient(MessageService, transport);
 
@@ -83,5 +84,29 @@ export const GetLatestMessage = async function* (
   for await (const message of response) {
     console.log("Message received", message);
     yield message;
+  }
+};
+
+export const GetHistoryMessage = async function (
+  roomId:bigint,
+  direction:Direction = Direction.OLDER,
+  limit:number = 10,
+):Promise<MessageList> {
+  const request = create(GetHistoryMessageRequestSchema, {
+    roomId: create(RoomIdSchema, {
+      id: roomId,
+    }),
+    limit: limit,
+    direction:direction,
+  });
+
+  // send the request
+  try {
+    const response = await client.getHistoryMessages(request);
+    console.log("Successfully get history message");
+    return response;
+  } catch (error) {
+    console.error("Unexpected error when trying to retrieve room list", error);
+    throw error;
   }
 };
