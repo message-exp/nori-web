ae8797ae2530c37028aea73059a25aa9a81670bb
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-04-02 11:00:51 +0800

    ⚰️ remove unnecessary path alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './app'),
-    },
-  },
 });
