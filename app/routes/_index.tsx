c06b3b4b80bd2414835a95072c749b07e209d408
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-04-02 10:44:47 +0800

    💄 create home and register page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/_index";
 import { Welcome } from "../welcome/welcome";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +11,22 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  // return <Welcome />;
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle>Welcome to nori!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <a href="/login" className="mr-3">
+            <Button variant="outline">Sign in</Button>
+          </a>
+          <a href="/register">
+            <Button variant="outline">Create an account</Button>
+          </a>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
---
+++ app/routes/register.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <p>Register</p>
+    </div>
+  );
+}
