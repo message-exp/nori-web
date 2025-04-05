aede31911aa64dd418b0eba3e2718cd816c0562a
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-04-05 15:15:37 +0800

    🧑‍💻 setup typescript-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  [globalIgnores([".react-router/*"])],
+);
