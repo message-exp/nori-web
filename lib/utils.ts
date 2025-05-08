154b35747a2bbbcc16d4d6354616be6a519a82cd
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-05-08 15:25:08 +0800

    ♻️ re-setup shadcn/ui

---
+++ lib/utils.ts
@@ -0,0 +1,87 @@
+import { clsx, type ClassValue } from "clsx";
+import Cookies from "js-cookie";
+import type {
+  IRefreshTokenResponse,
+  LoginResponse,
+  RegisterResponse,
+  Room,
+} from "matrix-js-sdk";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getLatestMessageText(room: Room) {
+  if (!room) return null;
+
+  const events = room.getLiveTimeline().getEvents();
+  if (!events || events.length === 0) return null;
+
+  // 倒著找最新的文字訊息
+  for (let i = events.length - 1; i >= 0; i--) {
+    const event = events[i];
+
+    if (event.getType() === "m.room.message") {
+      const content = event.getContent();
+      if (content.msgtype === "m.text") {
+        return content.body;
+      }
+    }
+  }
+
+  return null; // 沒有找到符合條件的訊息
+}
+
+export function setAuthCookies(
+  authResponse: LoginResponse | RegisterResponse | IRefreshTokenResponse,
+  baseUrl: string,
+) {
+  if (authResponse.access_token) {
+    Cookies.set("access_token", authResponse.access_token ?? "", {
+      expires: 7,
+      sameSite: "strict",
+      secure: false,
+    });
+  }
+  Cookies.set("refresh_token", authResponse.refresh_token ?? "", {
+    expires: 7,
+    sameSite: "strict",
+    secure: false,
+  });
+  if ("device_id" in authResponse && authResponse.device_id) {
+    Cookies.set("deviceId", authResponse.device_id, {
+      expires: 7,
+      sameSite: "strict",
+      secure: false,
+    });
+  }
+  if ("user_id" in authResponse && authResponse.user_id) {
+    Cookies.set("userId", authResponse.user_id, {
+      expires: 7,
+      sameSite: "strict",
+      secure: false,
+    });
+  }
+  Cookies.set("baseUrl", baseUrl, {
+    expires: 7,
+    sameSite: "strict",
+    secure: false,
+  });
+}
+
+export function getAuthCookies() {
+  const accessToken = Cookies.get("access_token");
+  const refreshToken = Cookies.get("refresh_token");
+  const deviceId = Cookies.get("deviceId");
+  const userId = Cookies.get("userId");
+  const baseUrl = Cookies.get("baseUrl");
+
+  return {
+    accessToken,
+    refreshToken,
+    deviceId,
+    userId,
+    baseUrl,
+  };
+}
