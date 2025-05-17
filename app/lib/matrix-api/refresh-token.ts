d5d2f48ab4bd1919935d2f459f42dd536ff014c3
hungsean <hungsean0419@gmail.com>
2025-05-17 14:23:37 +0800

    ✨ key of fixing access token error

--- app/lib/matrix-api/refresh-token.ts
+++ app/lib/matrix-api/refresh-token.ts
@@ -33,3 +33,44 @@ export async function refreshToken(): Promise<string> {
     return "REFRESH_FAILED";
   }
 }
+
+export async function checkClientState(): Promise<boolean> {
+  const clientCookiesState = await client.restoreClient(false);
+  if (!clientCookiesState) {
+    return false;
+  }
+
+  try {
+    let whoamiResponse;
+    try {
+      whoamiResponse = await client.client.whoami();
+    } catch (whoamiError) {
+      console.error("error during whoami call, refresh token:", whoamiError);
+      const refreshTokenResponse = await refreshToken();
+      console.log("refreshed finished, result: ", refreshTokenResponse);
+      if (refreshTokenResponse === "REFRESH_SUCCESS") {
+        // 如果刷新成功，再次嘗試獲取用戶ID
+        try {
+          whoamiResponse = await client.client.whoami();
+          console.log("second whoami response: ", whoamiResponse?.user_id);
+        } catch (secondWhoamiError) {
+          console.error("Error during second whoami call:", secondWhoamiError);
+          return false;
+        }
+      }
+    }
+
+    // 如果最終還是無法獲取用戶ID，返回false
+    if (!whoamiResponse?.user_id) {
+      console.warn("can't get user id after refresh, return false");
+      return false;
+    }
+
+    console.log("client state check passed, return true");
+    await client.sync();
+    return true;
+  } catch (error) {
+    console.error("Error checking client state:", error);
+    return false;
+  }
+}
