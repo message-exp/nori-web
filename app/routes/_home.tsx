00af1ed1d9c55f1904e735418fafd76704d7a7f9
Chun-Cheng Yu <61136202+Chun-Cheng@users.noreply.github.com>
2025-05-19 22:08:58 +0800

    ⚡️ prevent reload room-list when changing width

--- app/routes/_home.tsx
+++ app/routes/_home.tsx
@@ -11,67 +11,63 @@ export default function HomeLayout() {
   const isMobile = useIsMobile();
   const [showMobileList, setShowMobileList] = useState(true);
 
-  if (isMobile && !showMobileList) {
-    return (
-      <RoomProvider>
-        <Outlet context={{ isMobile, showMobileList, setShowMobileList }} />
-      </RoomProvider>
-    );
-  }
-
   return (
-    <div className="flex flex-row h-screen">
-      <div className="flex flex-col justify-between h-full p-2 border-r">
-        <div className="flex flex-col gap-2">
-          <NavLink to="/home">
-            {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-            {({ isActive, isPending, isTransitioning }) => (
-              <Button
-                variant="outline"
-                size="icon"
-                className={clsx("size-12", isActive && "bg-accent")}
-              >
-                <House className="size-6" />
-              </Button>
-            )}
-          </NavLink>
+    <RoomProvider>
+      {isMobile && !showMobileList ? (
+        <Outlet context={{ isMobile, showMobileList, setShowMobileList }} />
+      ) : (
+        <div className="flex flex-row h-screen">
+          <div className="flex flex-col justify-between h-full p-2 border-r">
+            <div className="flex flex-col gap-2">
+              <NavLink to="/home">
+                {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+                {({ isActive, isPending, isTransitioning }) => (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={clsx("size-12", isActive && "bg-accent")}
+                  >
+                    <House className="size-6" />
+                  </Button>
+                )}
+              </NavLink>
+            </div>
+            <div className="flex flex-col gap-2">
+              <NavLink to="/inbox">
+                {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+                {({ isActive, isPending, isTransitioning }) => (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={clsx("size-12", isActive && "bg-accent")}
+                  >
+                    <Inbox className="size-6" />
+                  </Button>
+                )}
+              </NavLink>
+              <NavLink to="/user">
+                {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
+                {({ isActive, isPending, isTransitioning }) => (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={clsx("size-12", isActive && "bg-accent")}
+                  >
+                    {/* <UserRound className="size-6" /> */}
+                    <Avatar className="rounded-sm">
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback className="rounded-sm">CN</AvatarFallback>
+                    </Avatar>
+                  </Button>
+                )}
+              </NavLink>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <Outlet context={{ isMobile, showMobileList, setShowMobileList }} />
+          </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <NavLink to="/inbox">
-            {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-            {({ isActive, isPending, isTransitioning }) => (
-              <Button
-                variant="outline"
-                size="icon"
-                className={clsx("size-12", isActive && "bg-accent")}
-              >
-                <Inbox className="size-6" />
-              </Button>
-            )}
-          </NavLink>
-          <NavLink to="/user">
-            {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-            {({ isActive, isPending, isTransitioning }) => (
-              <Button
-                variant="outline"
-                size="icon"
-                className={clsx("size-12", isActive && "bg-accent")}
-              >
-                {/* <UserRound className="size-6" /> */}
-                <Avatar className="rounded-sm">
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback className="rounded-sm">CN</AvatarFallback>
-                </Avatar>
-              </Button>
-            )}
-          </NavLink>
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 w-full">
-        <RoomProvider>
-          <Outlet context={{ isMobile, showMobileList, setShowMobileList }} />
-        </RoomProvider>
-      </div>
-    </div>
+      )}
+    </RoomProvider>
   );
 }
