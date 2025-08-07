7a2087a2f6531b75cced07cf25ad7751285c8bc1
BarneyWu <barney9211@gmail.com>
2025-08-07 18:01:26 +0800

    Feat: return type

---
+++ app/lib/contacts-server-api/bridge/telegram.ts
@@ -0,0 +1,39 @@
+import contactsApi from "..";
+import { getAuthCookies } from "../../utils";
+
+interface telegramLoginRequestCodeResponse {
+  message: string;
+}
+
+interface telegramLoginVerifyCodeResponse {
+  state: string;
+  username?: string;
+  phone?: string;
+}
+interface telegramLogoutRequestCodeResponse {
+  message: string;
+}
+
+export function telegramLoginRequestCode(
+  phone: string,
+): Promise<telegramLoginRequestCodeResponse> {
+  return contactsApi
+    .post("/api/bridge/telegram/users/login/code", {
+      phone,
+    })
+    .then((response) => response.data);
+}
+export function telegramLoginVerifyCode(
+  code: string,
+): Promise<telegramLoginVerifyCodeResponse> {
+  return contactsApi
+    .post("/api/bridge/telegram/users/login/code/verify", {
+      code,
+    })
+    .then((response) => response.data);
+}
+export function telegramLogout(): Promise<telegramLogoutRequestCodeResponse> {
+  return contactsApi
+    .post("/api/bridge/telegram/users/logout", {})
+    .then((response) => response.data);
+}
