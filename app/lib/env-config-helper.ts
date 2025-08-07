a24273d1fc924123f70416634c65900bf4e7c633
Wu,Po-Lin <143308716+Barney9211@users.noreply.github.com>
2025-08-07 22:12:10 +0800

    Update env-config-helper.ts

--- app/lib/env-config-helper.ts
+++ app/lib/env-config-helper.ts
@@ -1,3 +1,2 @@
 export const HOME_SERVER = import.meta.env.VITE_HOME_SERVER || "matrix.org";
-export const CONTACTS_SERVER =
-  import.meta.env.VITE_CONTACTS_SERVER || "matrix.org";
+export const CONTACTS_SERVER = import.meta.env.VITE_CONTACTS_SERVER || "matrix.org";
