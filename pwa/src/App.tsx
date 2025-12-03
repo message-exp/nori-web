a2387bb5668ae6e3336aaae8521151da125769cb
hungsean <hungsean0419@gmail.com>
2025-12-03 11:41:01 +0800

    🔧 Update import paths to include .tsx extension and remove obsolete spec

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import appLogo from "/favicon.svg";
-import PWABadge from "./PWABadge";
+import PWABadge from "./PWABadge.tsx";
 import "./App.css";
 
 function App() {
